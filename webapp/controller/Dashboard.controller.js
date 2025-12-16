c1481839c4954062f95f2fe757e7890a95853a1c
Gopi-04 <pvgopi04@gmail.com>
2025-12-16 15:49:44 +0530

    feat: Implement initial dashboard view with incident and risk counts, mock data, and routing.

---
+++ webapp/controller/Dashboard.controller.js
@@ -0,0 +1,65 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/UIComponent"
+], function (Controller, JSONModel, UIComponent) {
+    "use strict";
+
+    return Controller.extend("com.kaar.ehsm.controller.Dashboard", {
+        onInit: function () {
+            var oViewModel = new JSONModel({
+                IncidentCount: 0,
+                RiskCount: 0
+            });
+            this.getView().setModel(oViewModel);
+            this._loadDashboardData();
+        },
+
+        _loadDashboardData: function () {
+            var oModel = this.getOwnerComponent().getModel();
+            var oViewModel = this.getView().getModel();
+
+            // Count Incidents (Mock logic: just reading all and counting)
+            // In real OData we might use $count or $inlinecount
+            oModel.read("/ZEHSM_INCIDENT_GPSet", {
+                success: function (oData) {
+                    var iCount = oData.results ? oData.results.length : 0;
+                    oViewModel.setProperty("/IncidentCount", iCount);
+                },
+                error: function (oError) {
+                    console.error("Error reading Incidents", oError);
+                }
+            });
+
+            // Count Risks
+            oModel.read("/ZEHSM_RISK_GPSet", {
+                success: function (oData) {
+                    var iCount = oData.results ? oData.results.length : 0;
+                    oViewModel.setProperty("/RiskCount", iCount);
+                },
+                error: function (oError) {
+                    console.error("Error reading Risks", oError);
+                }
+            });
+        },
+
+        onIncidentPress: function () {
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            // oRouter.navTo("Incidents"); // We will create this route next
+        },
+
+        onRiskPress: function () {
+            // Placeholder for Risk Navigation
+        },
+
+        onLogout: function () {
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.navTo("Login");
+        },
+
+        onNavBack: function () {
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.navTo("Login");
+        }
+    });
+});
